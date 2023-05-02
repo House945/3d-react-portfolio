14cf6674580d8fd604ff32ce6f752eb5299083d0
Sebastian <seba.krawczyk94@gmail.com>
2023-05-02 11:34:36 +0200

    [REQ] three + updated Loader/Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,38 +20,15 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             {/* this is gonna need a little more realism */}
-            As a Full Stack Developer, I bring a wealth of experience and
-            expertise to the table. With my proficiency in both front-end and
-            back-end web development, I am able to create end-to-end solutions
-            that meet the unique needs of my clients.
-            <br className="sm:hidden" />
-            <br />
-            I have a deep understanding of a wide range of programming languages
-            and web frameworks, including HTML, CSS, JavaScript, React, Vue,
-            Python and FastAPI. I also have experience with database management,
-            APIs, and cloud platforms like Azure.
-            <br className="sm:hidden" />
-            <br />
-            One of my greatest strengths as a Full Stack Developer is my ability
-            to see the big picture while also paying attention to the smallest
-            details. I understand that a great web application requires more
-            than just functional code - it must also be visually appealing,
-            intuitive to use, and capable of handling high volumes of traffic
-            without slowing down.
-            <br className="sm:hidden" />
-            <br />
-            I am passionate about staying up-to-date with the latest
-            technologies and industry best practices, and I am always looking
-            for new challenges that will push me to grow and expand my skills.
-            Above all, I am committed to delivering top-quality solutions that
-            exceed my clients' expectations and help them achieve their business
-            goals.
-            <br className="sm:hidden" />
-            <br />
-            If you're looking for a Full Stack Developer who can bring your web
-            application vision to life, look no further. Let's connect and
-            discuss how I can help you create a solution that is efficient,
-            effective, and tailored to your specific needs.
+            As a Full Stack Developer, I specialize in both front-end and
+            back-end web development. I have expertise in HTML, CSS, JavaScript,
+            React, Vue, databases, and APIs. I excel at creating visually
+            appealing, intuitive, and scalable web applications. I am committed
+            to staying up-to-date with the latest technologies and industry best
+            practices, and dedicated to delivering high-quality solutions that
+            exceed my clients' expectations. If you're looking for a Full Stack
+            Developer who can bring your web application vision to life, let's
+            connect and discuss how I can help you achieve your business goals.
           </p>
         </div>
       </div>
