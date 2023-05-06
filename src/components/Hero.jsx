e5f1a58dd00cdd42f9671334eb53cb16dc542bbf
Sebastian <seba.krawczyk94@gmail.com>
2023-05-06 12:32:50 +0200

    fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,9 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      {/* <ComputersCanvas /> */}
+
+      <ComputersCanvas />
+
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div
